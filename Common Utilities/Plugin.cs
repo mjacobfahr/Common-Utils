c7d8422544679e9196346c83cb48de3fba77a54f
Miki_hero <100715076+Mikihero@users.noreply.github.com>
2024-12-29 19:17:41 +0100

    updated to exiled 9

--- Common Utilities/Plugin.cs
+++ Common Utilities/Plugin.cs
@@ -41,9 +41,9 @@ public class Plugin : Plugin<Config>
 
     public override string Author { get; } = "ExMod-Team";
 
-    public override Version Version { get; } = new(7, 2, 3);
+    public override Version Version { get; } = new(8, 0, 0);
 
-    public override Version RequiredExiledVersion { get; } = new(8, 11, 0);
+    public override Version RequiredExiledVersion { get; } = new(9, 1, 1);
 
     public override string Prefix { get; } = "CommonUtilities";
 
