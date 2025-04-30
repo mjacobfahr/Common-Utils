24ec89c342b4fc01b6aa084421185d6da5578706
Snivy Films <Snivy.Films@gmail.com>
2025-04-30 10:41:31 -0400

    Exiled 9.6 Update

--- Common Utilities/EventHandlers/PlayerHandlers.cs
+++ Common Utilities/EventHandlers/PlayerHandlers.cs
@@ -1,4 +1,6 @@
 using System.Collections.Specialized;
+using Exiled.Permissions;
+using Exiled.Permissions.Features;
 
 namespace Common_Utilities.EventHandlers;
 
@@ -12,6 +14,8 @@ using Exiled.API.Features.Roles;
 using Exiled.CustomItems.API.Features;
 using Exiled.Events.EventArgs.Interfaces;
 using Exiled.Events.EventArgs.Player;
+using LabApi.Features.Permissions;
+using LabApi.Features.Permissions.Providers;
 using PlayerRoles;
 using UnityEngine;
 
@@ -56,12 +60,12 @@ public class PlayerHandlers
             if (config.StartingInventories[ev.NewRole].Ammo == null || config.StartingInventories[ev.NewRole].Ammo.Count <= 0) 
                 return;
             
-            if (config.StartingInventories[ev.NewRole].Ammo.Any(s => string.IsNullOrEmpty(s.Group) || s.Group == "none" || (ServerStatic.PermissionsHandler._groups.TryGetValue(s.Group, out UserGroup userGroup) && userGroup == ev.Player.Group)))
+            if (config.StartingInventories[ev.NewRole].Ammo.Any(s => string.IsNullOrEmpty(s.Group) || s.Group == "none" || s.Group == ev.Player.Group.Name))
             {
                 ev.Ammo.Clear();
                 foreach ((ItemType type, ushort amount, string group) in config.StartingInventories[ev.NewRole].Ammo)
                 {
-                    if (string.IsNullOrEmpty(group) || group == "none" || (ServerStatic.PermissionsHandler._groups.TryGetValue(group, out UserGroup userGroup) && userGroup == ev.Player.Group))
+                    if (string.IsNullOrEmpty(group) || group == "none" || group == ev.Player.Group.Name)
                     {
                         ev.Ammo.Add(type, amount);
                     }
@@ -166,7 +170,7 @@ public class PlayerHandlers
                     player == null 
                     || string.IsNullOrEmpty(x.Group) 
                     || x.Group == "none" 
-                    || (ServerStatic.PermissionsHandler._groups.TryGetValue(x.Group, out var group) && group == player.Group))
+                    || x.Group == player.Group.Name)
                 .ToList();
 
             Log.Debug($"{nameof(GetStartingInventory)} Finished checking groups, found {itemChances.Count} valid itemChances.");
