ee7886fc109d07790b76bdff38a575a756df78d4
reid-sutherland <reid.sutherland96@gmail.com>
2025-08-03 15:10:19 -0600

    added some enum extensions

---
+++ CommonUtils/CommonUtils.Core/Extensions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CommonUtils.Core;
+
+public static class Extensions
+{
+    /////////////////////////////////////////////////////////
+    // Enum Extensions
+    /////////////////////////////////////////////////////////
+
+    // Not an extension but...
+    public static List<Enum> GetAllValues(Type type)
+    {
+        return Enum.GetValues(type).Cast<Enum>().ToList();
+    }
+
+    public static List<Enum> GetAllValues(this Enum value)
+    {
+        return Enum.GetValues(value.GetType()).Cast<Enum>().ToList();
+    }
+
+    public static IEnumerable<Enum> GetFlags(this Enum value)
+    {
+        return Enum.GetValues(value.GetType()).Cast<Enum>().ToArray();
+    }
+
+    public static IEnumerable<Enum> GetActiveFlags(this Enum value)
+    {
+        Enum[] flags = new Enum[0];
+        foreach (var flag in value.GetFlags())
+        {
+            if (value.HasFlag(flag))
+            {
+                flags = flags.Append(flag).ToArray();
+            }
+        }
+        return flags;
+    }
+
+    public static string GetActiveFlagNames(this Enum value)
+    {
+        return string.Join(", ", value.GetActiveFlags());
+    }
+}
